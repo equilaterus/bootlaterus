5cdbfa356b6cb7e005e308bb96fb1fbc766ab54e
Daniel Cañizares Corrales <dacanizares@outlook.com>
2018-10-10 22:55:55 -0500

    Added grunt with sass support

---
+++ gruntfile.js
@@ -0,0 +1,20 @@
+module.exports = function (grunt) {
+    const sass = require('node-sass');
+
+    require('load-grunt-tasks')(grunt);
+
+    grunt.initConfig({
+        sass: {
+            options: {
+                implementation: sass
+            },
+            dist: {
+                files: {
+                    'dist/css/bootlaterus.css': 'src/scss/bootlaterus.scss'
+                }
+            }
+        }
+    });
+
+    grunt.registerTask('default', ['sass']);
+}
