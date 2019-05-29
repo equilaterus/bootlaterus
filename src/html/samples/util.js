cb9bba80addc734af1ef0b2202a0d77cd457da2e
Daniel Cañizares Corrales <dacanizares@outlook.com>
2019-05-28 19:27:36 -0500

    Adding UI to preview diff themes
    Closes #84

--- src/html/samples/util.js
+++ src/html/samples/util.js
@@ -4,4 +4,30 @@
 // You don't need to include this in your project.
 // in order to use bootlaterus.
 
-const themes = $BUILD_THEMES;
+(function ($) {
+  $.bootlaterus = {};
+
+  $.bootlaterus.CreateThemeSelector = function () {
+    const themes = $BUILD_THEMES;
+    const themesHtml = Object.entries(themes).reduce((str, current) => {
+      const [key, value] = current;
+      str += `<a class="dropdown-item" href="#" data-theme-path="${value}">${key}</a>`;
+      return str;
+    }, '');
+
+    const innerHtml = `<li class="nav-item dropdown">
+                    <a class="nav-link dropdown-toggle" href="#" id="theme-dropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                      Choose Theme
+                    </a>
+                    <div class="dropdown-menu" aria-labelledby="theme-dropdown">
+                      ${themesHtml}
+                    </div>
+                  </li>`;
+
+    $('#theme-selector').html(innerHtml);
+  }
+})($);
+
+$(function() {
+  $.bootlaterus.CreateThemeSelector();
+});
