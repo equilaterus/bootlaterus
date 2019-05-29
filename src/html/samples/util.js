59d55105376d5fa46d4a2d68be5c8f7bf0041340
Daniel Cañizares Corrales <dacanizares@outlook.com>
2019-05-28 19:39:22 -0500

    Refactoring UI generation code

--- src/html/samples/util.js
+++ src/html/samples/util.js
@@ -4,30 +4,31 @@
 // You don't need to include this in your project.
 // in order to use bootlaterus.
 
-(function ($) {
-  $.bootlaterus = {};
+const bootlaterusUtils = {};
 
-  $.bootlaterus.CreateThemeSelector = function () {
-    const themes = $BUILD_THEMES;
-    const themesHtml = Object.entries(themes).reduce((str, current) => {
-      const [key, value] = current;
-      str += `<a class="dropdown-item" href="#" data-theme-path="${value}">${key}</a>`;
-      return str;
-    }, '');
+bootlaterusUtils.GetUI = function() {
+  const themes = $BUILD_THEMES;
+  const themesHtml = Object.entries(themes).reduce((str, current) => {
+    const [key, value] = current;
+    str += `<a class="dropdown-item" href="#" data-theme-path="${value}">${key}</a>`;
+    return str;
+  }, '');
 
-    const innerHtml = `<li class="nav-item dropdown">
-                    <a class="nav-link dropdown-toggle" href="#" id="theme-dropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                      Choose Theme
-                    </a>
-                    <div class="dropdown-menu" aria-labelledby="theme-dropdown">
-                      ${themesHtml}
-                    </div>
-                  </li>`;
+  return `<li class="nav-item dropdown">
+            <a class="nav-link dropdown-toggle" href="#" id="theme-dropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+              Choose Theme
+            </a>
+            <div class="dropdown-menu" aria-labelledby="theme-dropdown">
+              ${themesHtml}
+            </div>
+          </li>`;
+}
 
-    $('#theme-selector').html(innerHtml);
-  }
-})($);
+bootlaterusUtils.CreateThemeSelector = function() {
+  console.log(this.GetUI());
+  $('#theme-selector').html(this.GetUI());
+}
 
 $(function() {
-  $.bootlaterus.CreateThemeSelector();
+  bootlaterusUtils.CreateThemeSelector();
 });
