9d3a213f89ed8e32b0e70b00a2008e9f9ee62a41
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2019-07-26 04:57:10 +0000

    Deploy equilaterus/bootlaterus to github.com/equilaterus/bootlaterus.git:gh-pages

--- samples/util.js
+++ samples/util.js
@@ -11,11 +11,11 @@ bootlaterusUtils.ChangeTheme = function (path) {
   cssLink.replaceWith(`<link href="${path}" type="text/css" rel="stylesheet">`);
 }
 
-bootlaterusUtils.GetUI = function() {
+bootlaterusUtils.GetUI = function(isRootPath) {
   const themes = {"bootlaterus":"../css/bootlaterus.css","bootlaterus + fonts":"../css/bootlaterus-cfonts.css","bootlaterus default bootstrap":"../css/bootlaterus-default-bootstrap.css","bootlaterus docs":"../css/bootlaterus-docs.css","bootlaterus docs + fonts":"../css/bootlaterus-docs-cfonts.css","bootlaterus docs default bootstrap":"../css/bootlaterus-docs-default-bootstrap.css","bootlaterus docs light":"../css/bootlaterus-docs-light.css","bootlaterus docs light + fonts":"../css/bootlaterus-docs-light-cfonts.css","bootlaterus light":"../css/bootlaterus-light.css","bootlaterus light + fonts":"../css/bootlaterus-light-cfonts.css"};
   const themesHtml = Object.entries(themes).reduce((str, current) => {
     const [key, value] = current;
-    str += `<a class="dropdown-item" href="#" data-theme-path="${value}">${key}</a>`;
+    str += `<a class="dropdown-item" href="#" data-theme-path="${isRootPath ? value.replace('../', '') : value}">${key}</a>`;
     return str;
   }, '');
 
@@ -29,14 +29,15 @@ bootlaterusUtils.GetUI = function() {
           </li>`;
 }
 
-bootlaterusUtils.CreateThemeSelector = function(selector) {
-  $(selector).html(this.GetUI());
+bootlaterusUtils.CreateThemeSelector = function(selector, isRootPath) {
+  $(selector).html(this.GetUI(isRootPath));
 }
 
 
 
 $(function() {
-  bootlaterusUtils.CreateThemeSelector('#theme-selector');
+  const isRootPath = $('#theme-selector').data('is-root');
+  bootlaterusUtils.CreateThemeSelector('#theme-selector', isRootPath);
 
   $('a[data-theme-path]').click(function() {
     bootlaterusUtils.ChangeTheme($(this).data('theme-path'));
