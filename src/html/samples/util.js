cc62de19d588ec982c3bf6d231666f6a5c22edd7
dacanizares <dacanizares@outlook.com>
2019-08-19 16:35:12 -0500

    Added smooth scroll script
    Closes #172
    
    Co-Authored-By: Juan Felipe Cañizares Corrales <pipecaniza@outlook.com>

--- src/html/samples/util.js
+++ src/html/samples/util.js
@@ -39,7 +39,25 @@ $(function() {
   const isRootPath = $('#theme-selector').data('is-root');
   bootlaterusUtils.CreateThemeSelector('#theme-selector', isRootPath);
 
+  // Theme change handler
   $('a[data-theme-path]').click(function() {
     bootlaterusUtils.ChangeTheme($(this).data('theme-path'));
-  })
+  });
+
+  // Smooth scroll handler
+  $('a').on('click', function(event) {
+
+    if (!this.hash) 
+      return;
+
+    event.preventDefault();
+
+    let target = this.hash;
+    $('html, body').animate({
+      scrollTop: $(target).offset().top
+    }, 800, function(){
+      // Update URL
+      window.location.hash = hash;
+    });
+  });
 });
